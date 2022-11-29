fa759fa21203572ddf9747d31c153a72aec29edd
unknown <lmacumot@trf3.jus.br>
2022-11-29 16:48:47 -0300

    first commit

---
+++ ContatosMVC/Data/BancoContext.cs
@@ -0,0 +1,21 @@
+﻿using ContatosMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContatosMVC.Data
+{
+    public class BancoContext : DbContext
+    {
+        //COnstrutor
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options)
+        {
+
+        }
+
+        //Definir os DbSet - Configurar as tabelas (entidades)
+        //Classe ContatoModel servirá de base
+
+        public DbSet<ContatoModel> Contatos { get; set; }
+
+
+    }
+}
